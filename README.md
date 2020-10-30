9f1f16060239be19ef323da074beca25d8d7b1b2
lanasandra <sandra.cabessa@gmail.com>
2020-10-30 09:44:27 +0100

    Update files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# LegarantMobileApp
+
---
+++ app.js
@@ -0,0 +1,6 @@
+let signInLink = document.getElementById("signInLink");
+let loginForm = document.getElementById("loginForm");
+signInLink.addEventListener("click", () => {
+    loginForm.style.display = "none";
+    
+});

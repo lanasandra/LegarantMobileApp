b368f693d48e427c6fb8025f0f936e2ff3d409ae
lanasandra <sandra.cabessa@gmail.com>
2020-11-08 23:08:50 +0100

    new test update request

--- public/client.js
+++ public/client.js
@@ -1,24 +1,33 @@
 console.log('Client-side code running');
 
 // Variables declaration
-var emailInput = document.getElementById('inputEmail');
-var passwordInput = document.getElementById('inputPassword');
-const registerButton = document.getElementById('registerButton');
-const loginButton = document.getElementById("loginButton");
-var firstNameInput = document.getElementById("inputFirstName");
-var lastNameInput = document.getElementById("inputLastName");
-var footerLogin = document.getElementById("footerLogin");
-var footerRegister = document.getElementById("footerRegister");
-var createAccountTitle = document.getElementById("createAccountLogin");
-var loginTitle = document.getElementById("loginTitle");
-var loginLink = document.getElementById("loginLink");
-var registerLink = document.getElementById("signInLink");
-var blocRegister = document.getElementById("blocRegister");
-var blocLogin = document.getElementById("blocLogin");
-const updateButton = document.getElementById("updateButton");
-var blocProductDetails = document.getElementById("blocProductDetails")
-var welcomePage = document.getElementById("welcomePage");
-var informationsPage = document.getElementById("informationsPage")
+var emailInput          = document.getElementById('inputEmail');
+var passwordInput       = document.getElementById('inputPassword');
+const registerButton    = document.getElementById('registerButton');
+const loginButton       = document.getElementById("loginButton");
+var firstNameInput      = document.getElementById("inputFirstName");
+var lastNameInput       = document.getElementById("inputLastName");
+var footerLogin         = document.getElementById("footerLogin");
+var footerRegister      = document.getElementById("footerRegister");
+var createAccountTitle  = document.getElementById("createAccountLogin");
+var loginTitle          = document.getElementById("loginTitle");
+var loginLink           = document.getElementById("loginLink");
+var registerLink        = document.getElementById("signInLink");
+var blocRegister        = document.getElementById("blocRegister");
+var blocLogin           = document.getElementById("blocLogin");
+const updateButton      = document.getElementById("updateButton");
+var blocProductDetails  = document.getElementById("blocProductDetails");
+var welcomePage         = document.getElementById("welcomePage");
+var informationsPage    = document.getElementById("informationsPage");
+var contactFirstName    = document.getElementById("salesFirstName");
+var contactLastName     = document.getElementById("salesLastName");
+var contactEmail        = document.getElementById("salesEmail");
+var contactPhone        = document.getElementById("salesPhoneNumber");
+var contactStreet       = document.getElementById("salesStreet");
+var contactCity         = document.getElementById("salesCity");
+var contactCountry      = document.getElementById("salesCountry");
+var contactSalesforceId = document.getElementById("salesSalesforceId");
+
 /*-----------------------------------------------------------------------*/
 
 
@@ -172,14 +181,14 @@ updateButton.addEventListener('click', function(e){
 
     };
   xhr.send(JSON.stringify({
-    firstName: document.getElementById("salesFirstName").value,
-    lastName: document.getElementById("salesLastName").value,
-    email: document.getElementById("salesEmail").value,
-    phone: document.getElementById("salesPhoneNumber").value,
-    mailingStreet: document.getElementById("salesStreet").value,
-    mailingCity: document.getElementById("salesCity").value,
-    mailingCountry:  document.getElementById("salesCountry").value,
-    salesforcId:  document.getElementById("salesSalesforceId").value
+    firstName:      contactFirstName.value,
+    lastName:       contactLastName.value,
+    email:          contactEmail.value,
+    phone:          contactPhone.value,
+    mailingStreet:  contactStreet.value,
+    mailingCity:    contactCity.value,
+    mailingCountry: contactCountry.value,
+    salesforcId:    contactSalesforceId.value
     }));
 });
 

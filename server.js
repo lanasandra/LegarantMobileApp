9553a35221c059d957b1f7fe19c38787a42b5fe8
lanasandra <sandra.cabessa@gmail.com>
2020-11-14 17:43:15 +0100

    environnement file config

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const pg = require('pg');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.set('port', process.env.PORT || 5432);
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
@@ -15,16 +15,15 @@ app.use(express.json());
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded({ extended: true }))
 
+
 // Creation of the connection to postgres
 const { Client } = require('pg');
-const { parse } = require('path');
-const { REFUSED } = require('dns');
 const client = new Client({
-  host: 'ec2-54-217-224-85.eu-west-1.compute.amazonaws.com',
-  port: 5432,
-  user: 'zxfnomexlvhdqe',
-  password: '1b77e7564cb41f35805ab0bb52f7b6afce58348bcb8ed794af456bf986f2bf40',
-  database: 'd31rv5qg5uqjs1',
+  host :process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   ssl: {
     rejectUnauthorized: false
   }
@@ -82,7 +81,7 @@ app.post('/api/getProducts', (req, res) => {
   // Query to retreive the products details where pricebook is Legarant pricebook
 const query = {
   text: 'SELECT productcode, name, unitprice from Salesforce.PriceBookEntry where pricebook2id=$1 order by name',
-  values: ['01s09000001emDjAAI']
+  values: [process.env.PRICEBOOK_LEGARANT]
 
 }
   client.query(query).then(response => {
@@ -96,8 +95,7 @@ const query = {
   })
 });
 
-
-// Request to set the password when creating an new account
+// Request to set the password when creating a new account
 app.post('/api/register', (req, res) => {
   
   const query = {
@@ -133,5 +131,5 @@ app.post('/api/update', (req, res) => {
 });
   
 app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port ' + process.env.PORT);
 });
